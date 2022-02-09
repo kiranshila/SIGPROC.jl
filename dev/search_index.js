82593bbf9e305098ead8bf822d576d7b493123a8
Documenter.jl <documenter@juliadocs.github.io>
2022-02-09 19:50:12 +0000

    build based on 24be291

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SIGPROC","category":"page"},{"location":"#SIGPROC","page":"Home","title":"SIGPROC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SIGPROC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SIGPROC]","category":"page"}]
+}
