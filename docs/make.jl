a4692252fa62988e9deff638813ee87ce59e92cc
Kiran Shila <me@kiranshila.com>
2022-03-27 12:40:05 -0700

    Remove all the useless deps

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,7 @@ makedocs(;
          modules=[SIGPROC],
          authors="Kiran Shila <me@kiranshila.com> and contributors",
          repo="https://github.com/kiranshila/SIGPROC.jl/blob/{commit}{path}#{line}",
-         sitename="RadioTransients.jl",
+         sitename="SIGPROC.jl",
          format=Documenter.HTML(;
                                 prettyurls=get(ENV, "CI", "false") == "true",
                                 canonical="https://kiranshila.github.io/SIGPROC.jl",
